105a90a1dfb4b5fd1c04e5a70cdc5bdd72c932c3
Erick Hilda Andreas <erick.hilda@ajaib.co.id>
2023-03-18 21:26:03 +0700

    feat: add label element property

--- src/data/label.ts
+++ src/data/label.ts
@@ -1,3 +1,4 @@
+import { ChemicalElement } from '@/types/global';
 export const categories = [
   'actinide',
   'alkali metal',
@@ -10,3 +11,21 @@ export const categories = [
   'post-transition metal',
   'transition metal',
 ] as const;
+
+export const propertyLabels: Partial<
+  Record<keyof ChemicalElement, [string, string]>
+> = {
+  atomic_mass: ['Atomic Mass', 'u'],
+  atomic_radius: ['Atomic Radius', 'pm'],
+  boiling_point: ['Boiling Point', 'K'],
+  covalent_radius: ['Covalent Radius', 'pm'],
+  density: ['Density', 'g/cm³'],
+  electronegativity: ['Electronegativity', ''],
+  electron_affinity: ['Electron Affinity', 'kJ/mol'],
+  first_ionization: ['First Ionization', 'kJ/mol'],
+  melting_point: ['Melting Point', 'K'],
+  molar_heat: ['Molar Heat', 'J/(mol·K)'],
+  n_valence: ['Electron valency', ''],
+  shells: ['Shells ocupation', ''],
+  specific_heat: ['Specific Heat', 'J/(g·K)'],
+};
