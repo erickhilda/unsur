0d13231bfa5bb431ca34455c2988e73cf9c9a4b8
Erick Hilda Andreas <erick.hilda@ajaib.co.id>
2023-03-18 21:27:53 +0700

    feat: add layout component

---
+++ src/components/layout/nav-bar.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import Icon from '../general/Icon';
+
+function NavBar() {
+  return (
+    <nav className="sticky top-0 z-10 flex items-center justify-between h-16 backdrop-filter backdrop-blur-sm mx-16">
+      <Link href="/" className="flex items-center">
+        <Icon icon="ph:flask-duotone" width={32} height={32} />
+      </Link>
+      <div className="flex items-center gap-6">
+        <Link href="/acknowledment">
+          <span>Acknowledgements</span>
+        </Link>
+        <Link href="https://github.com/erickhilda/unsur">
+          <span>Github</span>
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
