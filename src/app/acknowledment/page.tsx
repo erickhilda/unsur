5dcc6938cc71bbec005b92d2be43377e3bd09337
Erick Hilda Andreas <erick.hilda@ajaib.co.id>
2023-03-18 21:29:23 +0700

    feat: add akcnowledment page

---
+++ src/app/acknowledment/page.tsx
@@ -0,0 +1,11 @@
+import NavBar from '@/components/layout/nav-bar';
+
+function Acknowledgement() {
+  return (
+    <div>
+      <p>Thanks for your order!</p>
+    </div>
+  );
+}
+
+export default Acknowledgement;
