f3db97c60dce4ae302f39820080e70320a481ba3
Erick Hilda Andreas <erick.hilda@ajaib.co.id>
2023-03-30 18:33:57 +0700

    feat: make the layout responsive for mobile device

--- src/components/layout/nav-bar.tsx
+++ src/components/layout/nav-bar.tsx
@@ -4,7 +4,7 @@ import Icon from '../general/icon';
 function NavBar() {
   return (
     <nav className="sticky top-0 z-10 h-16 backdrop-filter backdrop-blur-sm">
-      <div className="flex items-center justify-between mx-16 h-full">
+      <div className="flex items-center justify-between lg:mx-16 mx-3 h-full">
         <Link href="/" className="flex items-center">
           <Icon icon="ph:flask-duotone" width={32} height={32} />
         </Link>
